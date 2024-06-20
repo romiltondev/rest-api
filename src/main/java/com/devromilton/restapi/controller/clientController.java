2d88cea0acb3d1f0baf6fe6b290a952040e2ad41
Romilton Costa <156550707+romiltondev@users.noreply.github.com>
2024-06-20 21:08:07 +0000

    update

--- src/main/java/com/devromilton/restapi/controller/clientController.java
+++ src/main/java/com/devromilton/restapi/controller/clientController.java
@@ -3,6 +3,7 @@ package com.devromilton.restapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +26,7 @@ public class clientController {
         return clientrepository.findAll();
     }
     @PostMapping
-    @ResponseStatus
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Cliente adicionar(@RequestBody Cliente cliente){
         return clientrepository.save(cliente);
     }
