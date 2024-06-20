8f22a40ab048ead08594ebdd83353c7d1ced65dc
Romilton Costa <156550707+romiltondev@users.noreply.github.com>
2024-06-20 20:33:39 +0000

    feat:create clientController

--- src/main/java/com/devromilton/restapi/controller/clientController.java
+++ src/main/java/com/devromilton/restapi/controller/clientController.java
@@ -2,18 +2,23 @@ package com.devromilton.restapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devromilton.restapi.model.cliente;
+import com.devromilton.restapi.model.Cliente;
+import com.devromilton.restapi.repository.ClientRepository;
 
 @RestController
 @RequestMapping("/clients")
 public class clientController {
 
-    @GetMapping
-    public List<cliente> listar(){
+    @Autowired
+    private ClientRepository clientrepository;
 
+    @GetMapping
+    public List<Cliente> listar(){
+        return clientrepository.findAll();
     }
 }
